d4161b8b80111a5df74c002e8439b14eea8095e0
Xolani Maphumulo <xolani.maphumulo@mohara.co>
2024-07-02 00:47:17 +0200

    chore:added shadcn

---
+++ src/app/(use_state)/use-state/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+const UsingUseState = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+export default UsingUseState;
